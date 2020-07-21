245aeb088d3ac28cc7010fbeb9f515a94f9d34ea
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-07-21 20:04:08 -0300

    feat: finish delivery

--- src/repositories/FilesRepository.ts
+++ src/repositories/FilesRepository.ts
@@ -4,6 +4,12 @@ import File from '../models/File';
 
 @EntityRepository(File)
 class FileRepository extends Repository<File> {
+  public async findById(id: string): Promise<File | undefined> {
+    const findFile = await this.findOne({ where: { id } });
+
+    return findFile;
+  }
+
   public async findByPath(path: string): Promise<File | undefined> {
     const findFile = await this.findOne({ where: { path } });
 
--- src/routes/deliveryman.routes.ts
+++ src/routes/deliveryman.routes.ts
@@ -4,6 +4,7 @@ import DeliverymanController from '../controllers/DeliverymanController';
 import DeliveryDeliveredController from '../controllers/DeliveryDeliveredController';
 import DeliveryPendingController from '../controllers/DeliveryPendingController';
 import DeliveryWithdrawController from '../controllers/DeliveryWithdrawController';
+import DeliveryFinishController from '../controllers/DeliveryFinishController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -12,12 +13,17 @@ const deliverymanController = new DeliverymanController();
 const deliveryDeliveredController = new DeliveryDeliveredController();
 const deliveryPendingController = new DeliveryPendingController();
 const deliveryWithdrawController = new DeliveryWithdrawController();
+const deliveryFinishController = new DeliveryFinishController();
 
 deliverymanRouter.get('/:id/deliveries', deliveryDeliveredController.index);
 deliverymanRouter.get('/:id', deliveryPendingController.index);
 deliverymanRouter.post(
   '/:deliveryman_id/delivery/:delivery_id',
   deliveryWithdrawController.store,
+);
+deliverymanRouter.post(
+  '/:deliveryman_id/delivery/:delivery_id/finish',
+  deliveryFinishController.store,
 );
 deliverymanRouter.use(ensureAuthenticated);
 deliverymanRouter.get('/', deliverymanController.index);
