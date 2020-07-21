44aab75e042b830c3120fba1d1326890baa126fe
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-07-21 00:17:32 -0300

    feat: create ensure authenticated middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 
 import UsersController from '../controllers/UsersController';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
 const usersController = new UsersController();
 
+usersRouter.use(ensureAuthenticated);
 usersRouter.post('/', usersController.store);
 
 export default usersRouter;
