823334a72da4730badcee5e1cb902d6ef24ad4a0
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-07-22 07:29:30 -0300

    feat: list deliveries with pagination and filter

--- src/controllers/delivery/DeliveriesController.ts
+++ src/controllers/delivery/DeliveriesController.ts
@@ -5,13 +5,39 @@ import DeleteDeliveryService from '../../services/delivery/DeleteDeliveryService
 import CreateDeliveryService from '../../services/delivery/CreateDeliveryService';
 import UpdateDeliveryService from '../../services/delivery/UpdateDeliveryService';
 
+interface IListDeliveries {
+  product_name?: string;
+  page: number;
+  limit: number;
+}
+
 class DeliveriesController {
-  public async index(_request: Request, response: Response): Promise<Response> {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { q: product_name, page = 1, limit = 5 } = request.query;
+
     const listDeliveries = new ListDeliveriesService();
 
-    const deliveries = await listDeliveries.execute();
+    const {
+      deliveries,
+      page_count,
+      current_page,
+      per_page,
+      total_items,
+      total_pages,
+    } = await listDeliveries.execute({
+      product_name,
+      page: Number(page),
+      limit: Number(limit),
+    } as IListDeliveries);
 
-    return response.status(200).json(deliveries);
+    return response.status(200).json({
+      deliveries,
+      page_count,
+      current_page,
+      per_page,
+      total_items,
+      total_pages,
+    });
   }
 
   public async store(request: Request, response: Response): Promise<Response> {
--- src/services/delivery/ListDeliveriesService.ts
+++ src/services/delivery/ListDeliveriesService.ts
@@ -1,18 +1,53 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Like } from 'typeorm';
 
 import DeliveriesRepository from '../../repositories/DeliveriesRepository';
 
 import Delivery from '../../models/Delivery';
 
+interface Request {
+  product_name?: string;
+  page: number;
+  limit: number;
+}
+
+interface Response {
+  deliveries: Delivery[];
+  page_count: number;
+  total_pages: number;
+  current_page: number;
+  total_items: number;
+  per_page: number;
+}
+
 class ListDeliveriesService {
-  public async execute(): Promise<Delivery[]> {
+  public async execute({
+    product_name,
+    page,
+    limit,
+  }: Request): Promise<Response> {
     const deliveriesRepository = getCustomRepository(DeliveriesRepository);
 
-    const deliveries = await deliveriesRepository.find({
+    let where = {};
+
+    if (product_name) {
+      where = { product: Like(`%${product_name}%`) };
+    }
+
+    const [deliveries, count] = await deliveriesRepository.findAndCount({
+      where,
+      skip: page * limit - limit,
+      take: limit,
       relations: ['recipient', 'deliveryman', 'signature'],
     });
 
-    return deliveries;
+    return {
+      deliveries,
+      page_count: deliveries.length,
+      current_page: page,
+      per_page: limit,
+      total_items: count,
+      total_pages: Math.ceil(count / limit),
+    };
   }
 }
 
